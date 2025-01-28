939da72eca53ead84101eae0c660599dda6154db
Faiq Haikal <faiqisamazing@gmail.com>
2025-01-28 23:08:34 +0900

    uibuild

---
+++ backend/models/person.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+mongoose.set('strictQuery', false)
+
+console.log('connecting to database')
+mongoose.connect(url)
+.then(result => {
+    console.log('connected to MongoDB')
+})
+.catch(error => console.log('error connecting to MongoDB:', error.message)
+)
+
+const phonebookSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        minLength: 3,
+        required: true,
+    },
+    number: String,
+})
+
+phonebookSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Person', phonebookSchema)
+
